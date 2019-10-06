94d973fb836939adf2b211dbb80c2b6b5706f112
shvv733kn@gmail.com <shvv733kn@gmail.com>
2019-10-06 14:09:20 +0300

    db

--- db/migrate/20190913131817_create_tariff_plans.rb
+++ db/migrate/20190913131817_create_tariff_plans.rb
@@ -1,12 +1,12 @@
-# class CreateTariffPlans < ActiveRecord::Migration[5.2]
-#   def change
-#     create_table :tariff_plans do |t|
-#       t.integer :price
-#       t.integer :months
-#       t.string :name
-#       t.string :description
+class CreateTariffPlans < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tariff_plans do |t|
+      t.integer :price
+      t.integer :months
+      t.string :name
+      t.string :description
 
-#       t.timestamps
-#     end
-#   end
-# end
+      t.timestamps
+    end
+  end
+end
--- db/migrate/20190913134721_create_tariff_plan_purchases.rb
+++ db/migrate/20190913134721_create_tariff_plan_purchases.rb
@@ -1,11 +1,11 @@
-# class CreateTariffPlanPurchases < ActiveRecord::Migration[5.2]
-#   def change
-#     create_table :tariff_plan_purchases do |t|
-#       t.integer :tariff_plan_id
-#       t.date :expired_at
+class CreateTariffPlanPurchases < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tariff_plan_purchases do |t|
+      t.integer :tariff_plan_id
+      t.date :expired_at
 
-#       t.timestamps
-#     end
-#     rename_column :companies, :tariff_plan_id, :tariff_plan_purchase_id
-#   end
-# end
+      t.timestamps
+    end
+    rename_column :companies, :tariff_plan_id, :tariff_plan_purchase_id
+  end
+end
