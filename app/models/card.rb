c1bae4b5c4058f0942d66721b01407652ef58dd9
shvv733kn@gmail.com <shvv733kn@gmail.com>
2019-12-26 13:52:45 +0300

    cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,8 @@
 class Card < ApplicationRecord
-    validates :number, length: {minimum: 1, maximum: 64}, uniqueness: true
+    validates :number, length: {minimum: 1, maximum: 64}#, uniqueness: true
     validates :points, inclusion: 0..10000000000
+
+    validates_uniqueness_of :number, scope: :company_id
 
     belongs_to :company
     #belongs_to :operator
