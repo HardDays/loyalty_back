ef2c73ef5d1b4929f26ebad66fe197a8763c2b4c
shvv733kn@gmail.com <shvv733kn@gmail.com>
2019-12-26 13:47:16 +0300

    cards

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,13 +1,13 @@
 module Api
     module V1
       class CardsController < ApplicationController
-        before_action :auth_operator, only: [:create, :create_range]
+        before_action :auth_creator, only: [:create, :create_range]
   
         # POST /cards
         def create
           card = Card.new(card_params)
-          card.company = @auth_user.operator.company
-          card.operator = @auth_user.operator
+          card.company = @auth_user.creator.company
+          #card.creator = @auth_user.creator
           if card.save
             render json: card
           else
@@ -21,8 +21,8 @@ module Api
             (params[:begin_range].to_i..params[:end_range].to_i).each do |i|
               card = Card.new(card_params)
               card.number = "%010d" % i
-              card.company = @auth_user.operator.company
-              card.operator = @auth_user.operator
+              card.company = @auth_user.creator.company
+              #card.creator = @auth_user.creator
               card.save
             end
           end
@@ -34,9 +34,9 @@ module Api
             @auth_user = User.authorize(request.headers['Authorization'])
           end
   
-          def auth_operator
+          def auth_creator
             auth
-            @auth_user.role(@auth_user.operator_role)
+            @auth_user.role(@auth_user.creator_role)
           end
   
           def card_params
--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,5 @@ class Card < ApplicationRecord
     validates :points, inclusion: 0..10000000000
 
     belongs_to :company
-    belongs_to :operator
-
+    #belongs_to :operator
 end
