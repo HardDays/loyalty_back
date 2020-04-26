72b0247cee48a55a120abbf84a958ce99e2f12d7
hz <shvv733kn@gmail.com>
2020-04-26 13:53:30 +0300

    hz

--- app/helpers/vk_helper.rb
+++ app/helpers/vk_helper.rb
@@ -6,35 +6,37 @@ module VkHelper
         groups = VkGroup.all
         
         groups.each do |group|
-            id = -(group.group_id.to_i)
+            begin
+                id = -(group.group_id.to_i)
+                vk = VkontakteApi::Client.new(TOKEN)
+                posts = vk.wall.get(owner_id: id, v: '5.103').items
+                puts '##############'
+                puts id
+                puts json: posts
+                puts '##############'
 
-            vk = VkontakteApi::Client.new(TOKEN)
-            posts = vk.wall.get(owner_id: id, v: '5.103').items
-            puts '##############'
-            puts id
-            puts json: posts
-            puts '##############'
-
-            posts.each do |p|
-                likes = vk.likes.getList(type: 'post', owner_id: id, item_id: p.id, v: '5.103').items
-                likes.each do |like|
-                    client = Client.find_by(vk_id: like)
-                    if client
-                        event = VkEvent.new(client_id: client.id, vk_group_id: group.id, event_type: :wall_like, post_id: p.id)
-                        if event.save
-                            points = ClientPoint.new(
-                                current_points: group.wall_like_points,
-                                initial_points: group.wall_like_points,
-                                burning_date: DateTime.now + 100.years,
-                                activation_date: DateTime.now,
-                                client_id: client.id,
-                                points_source: :vk
-                            )
-                            points.save
+                posts.each do |p|
+                    likes = vk.likes.getList(type: 'post', owner_id: id, item_id: p.id, v: '5.103').items
+                    likes.each do |like|
+                        client = Client.find_by(vk_id: like)
+                        if client
+                            event = VkEvent.new(client_id: client.id, vk_group_id: group.id, event_type: :wall_like, post_id: p.id)
+                            if event.save
+                                points = ClientPoint.new(
+                                    current_points: group.wall_like_points,
+                                    initial_points: group.wall_like_points,
+                                    burning_date: DateTime.now + 100.years,
+                                    activation_date: DateTime.now,
+                                    client_id: client.id,
+                                    points_source: :vk
+                                )
+                                points.save
+                            end
                         end
                     end
                 end
             end
+        rescue => ex
         end
     end
 end
